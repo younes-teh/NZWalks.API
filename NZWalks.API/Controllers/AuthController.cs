b6386e2da4a21e584dfcf041f61c517752f88422
younes-teh <younes.boualam66@gmail.com>
2026-06-19 10:50:20 +0100

    Authentication and Authorization

--- NZWalks.API/Controllers/AuthController.cs
+++ NZWalks.API/Controllers/AuthController.cs
@@ -1,89 +1,89 @@
-﻿//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
-//using NZWalks.API.Models.DTO;
-//using NZWalks.API.Repositories;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
 
-//namespace NZWalks.API.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class AuthController : ControllerBase
-//    {
-//        private readonly UserManager<IdentityUser> userManager;
-//        private readonly ITokenRepository tokenRepository;
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly ITokenRepository tokenRepository;
 
-//        public AuthController(UserManager<IdentityUser> userManager, ITokenRepository tokenRepository)
-//        {
-//            this.userManager = userManager;
-//            this.tokenRepository = tokenRepository;
-//        }
-
-
-//        // POST: /api/Auth/Register
-//        [HttpPost]
-//        [Route("Register")]
-//        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
-//        {
-//            var identityUser = new IdentityUser
-//            {
-//                UserName = registerRequestDto.Username,
-//                Email = registerRequestDto.Username
-//            };
-
-//            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDto.Password);
-
-//            if (identityResult.Succeeded)
-//            {
-//                // Add roles to this User
-//                if (registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
-//                {
-//                    identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
-
-//                    if (identityResult.Succeeded)
-//                    {
-//                        return Ok("User was registered! Please login.");
-//                    }
-//                }
-//            }
-
-//            return BadRequest("Something went wrong");
-//        }
+        public AuthController(UserManager<IdentityUser> userManager, ITokenRepository tokenRepository)
+        {
+            this.userManager = userManager;
+            this.tokenRepository = tokenRepository;
+        }
 
 
-//        // POST: /api/Auth/Login
-//        [HttpPost]
-//        [Route("Login")]
-//        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
-//        {
-//            var user = await userManager.FindByEmailAsync(loginRequestDto.Username);
+        // POST: /api/Auth/Register
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username
+            };
 
-//            if (user != null)
-//            {
-//                var checkPasswordResult = await userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDto.Password);
 
-//                if (checkPasswordResult)
-//                {
-//                    // Get Roles for this user
-//                    var roles = await userManager.GetRolesAsync(user);
+            if (identityResult.Succeeded)
+            {
+                // Add roles to this User
+                if (registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
 
-//                    if (roles != null)
-//                    {
-//                        // Create Token
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User was registered! Please login.");
+                    }
+                }
+            }
 
-//                        var jwtToken = tokenRepository.CreateJWTToken(user, roles.ToList());
+            return BadRequest("Something went wrong");
+        }
 
-//                        var response = new LoginResponseDto
-//                        {
-//                            JwtToken = jwtToken
-//                        };
 
-//                        return Ok(response);
-//                    }
-//                }
-//            }
+        // POST: /api/Auth/Login
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
+        {
+            var user = await userManager.FindByEmailAsync(loginRequestDto.Username);
 
-//            return BadRequest("Username or password incorrect");
-//        }
-//    }
-//}
+            if (user != null)
+            {
+                var checkPasswordResult = await userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+
+                if (checkPasswordResult)
+                {
+                    // Get Roles for this user
+                    var roles = await userManager.GetRolesAsync(user);
+
+                    if (roles != null)
+                    {
+                        // Create Token
+
+                        var jwtToken = tokenRepository.CreateJWTToken(user, roles.ToList());
+
+                        var response = new LoginResponseDto
+                        {
+                            JwtToken = jwtToken
+                        };
+
+                        return Ok(response);
+                    }
+                }
+            }
+
+            return BadRequest("Username or password incorrect");
+        }
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -57,43 +57,45 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksAuthConne
 
 builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
 builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
-//builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+builder.Services.AddScoped<ITokenRepository, TokenRepository>();
 //builder.Services.AddScoped<IImageRepository, LocalImageRepository>();
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
 
-//builder.Services.AddIdentityCore<IdentityUser>()
-//    .AddRoles<IdentityRole>()
-//    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("NZWalks")
-//    .AddEntityFrameworkStores<NZWalksAuthDbContext>()
-//    .AddDefaultTokenProviders();
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("NZWalks")
+    .AddEntityFrameworkStores<NZWalksAuthDbContext>()
+    .AddDefaultTokenProviders();
 
 
-//builder.Services.Configure<IdentityOptions>(options =>
-//{
-//    options.Password.RequireDigit = false;
-//    options.Password.RequireLowercase = false;
-//    options.Password.RequireNonAlphanumeric = false;
-//    options.Password.RequireUppercase = false;
-//    options.Password.RequiredLength = 6;
-//    options.Password.RequiredUniqueChars = 1;
-//});
 
 
-//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-//    .AddJwtBearer(options =>
-//    options.TokenValidationParameters = new TokenValidationParameters
-//    {
-//        ValidateIssuer = true,
-//        ValidateAudience = true,
-//        ValidateLifetime = true,
-//        ValidateIssuerSigningKey = true,
-//        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-//        ValidAudience = builder.Configuration["Jwt:Audience"],
-//        IssuerSigningKey = new SymmetricSecurityKey(
-//            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-//    });
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    });
 
 var app = builder.Build();
 
